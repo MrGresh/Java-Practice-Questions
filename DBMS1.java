8a3347e638950245f90c5df7e8e8da58f6d76a22
Mr. Gresh <94007478+MrGresh@users.noreply.github.com>
2021-12-11 08:49:02 +0530

    Add files via upload

---
+++ DBMS1.java
@@ -0,0 +1,19 @@
+import java.sql.*;
+public class DBMS1 {
+    public static void main(String[] args)  {
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/employ","root","");
+            Statement obj=con.createStatement();
+            ResultSet rs=obj.executeQuery("select * from edetail");
+            System.out.println("connection successful");
+        }
+        catch(Exception e)
+        {
+
+            System.out.println("connection failed"+e);
+        }
+
+
+    }
+}
