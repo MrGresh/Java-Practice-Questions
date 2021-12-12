ca57bb2d10e2c09d5618af4efbf770bc7a2d5d00
Mr. Gresh <94007478+MrGresh@users.noreply.github.com>
2021-12-12 20:51:57 +0530

    Add files via upload

---
+++ CollectionHashSet.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class CollectionHashSet {
+    public static void main(String[] args) {
+        HashSet i = new HashSet<>();
+        System.out.println(i.add("neeraj")); //true
+        System.out.println(i.add("neeraj")); //false}
+
+    }
+}
